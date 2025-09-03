ed3b4247dfd741dd6d14497998a38f5c32b77cf8
Mezana <mezana1411@gmail.com>
2025-09-03 20:58:26 +0300

    Création projet

---
+++ src/main/java/com/example/grossi/GrossiApplication.java
@@ -0,0 +1,13 @@
+package com.example.grossi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrossiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrossiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/grossi/GrossiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.grossi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrossiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
